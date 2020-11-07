4e6695dd0698943d677d272d0683753a330e09b5
zakuraevs <zakuraevs@gmail.com>
2020-11-07 17:06:55 +0200

    update

--- hello/views.py
+++ hello/views.py
@@ -4,10 +4,28 @@ from django.shortcuts import render
 posts = [
     {
         'author': 'HK Scan',
-        'title': 'Calves separated from mothers so far:',
+        'title': 'Product:',
+        'product_name': 'Tenderloin Steak',
+        'media': 'https://hkruokatalo.studio.crasman.fi/pub/Kuvat/Tuotekuvat2/3661.jpg?c=system_1024x',
         'content': '69420',
         'date_posted': 'Nov 7 2020'
     }
+    # {
+    #     'author': 'HK Scan',
+    #     'title': 'Production location:',
+    #     'product_name': 'Tampere',
+    #     'media': '',
+    #     'content': '69420',
+    #     'date_posted': 'Nov 7 2020'
+    # },
+    # {
+    #     'author': 'HK Scan',
+    #     'title': 'Product name:',
+    #     'product_name': 'Tenderloin Steak',
+    #     'media': '',
+    #     'content': '69420',
+    #     'date_posted': 'Nov 7 2020'
+    # },
 ]
 
 def hello(request):
