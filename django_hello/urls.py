98a6b96bddb9489baeafca9c0ad43ed85341afa2
Son Le <42708725+SonAlexLe@users.noreply.github.com>
2020-11-07 20:21:06 +0200

    first migration

--- django_hello/urls.py
+++ django_hello/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 
-#path('admin/', admin.site.urls),
 
-urlpatterns = [    
+urlpatterns = [
+    path('admin/', admin.site.urls),    
     path('', include('hello.urls')),
     path('recipes', include('hello.urls')),
     path('products', include('hello.urls')),
