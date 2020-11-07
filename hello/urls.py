483d652a152f7e5ff95ad8f079fe53e8a7db7741
atreyaray@gmail.com <atreyaray@gmail.com>
2020-11-07 20:02:40 +0200

    corrections

--- hello/urls.py
+++ hello/urls.py
@@ -5,13 +5,9 @@ from . import views
 urlpatterns = [    
     path('', views.hello, name='hello'),
     path('pork', views.pork, name='pork'),
-<<<<<<< HEAD
-    path('products', views.products, name='products_page'),
-    path('about', views.about, name='about_page')
-=======
     path('beef', views.beef, name='beef'),
     path('chicken', views.chicken, name='chicken'),
-    path('products', views.products, name='products_page')
+    path('products', views.products, name='products_page'),
+    path('about', views.about, name='about_page')
 
->>>>>>> ca27b0256ecf511631ea5ccd17b014cd854785e1
 ]
