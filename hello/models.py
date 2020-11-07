b719005fc5de6e9629f8d2bbd32b4b9ae70592b9
Son Le <42708725+SonAlexLe@users.noreply.github.com>
2020-11-07 20:35:53 +0200

    implemented a database

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Product(models.Model):
+    productType = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=200)
+    description = models.TextField()
+    Recipe = models.TextField()
+    RecipeLink = models.TextField()
+    media = models.TextField()
+    Price = models.DecimalField(decimal_places=4, max_digits=100)
+    recipe_name = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    recipe_main = models.TextField()
+
+    def __str__(self):
+        return f"{self.productType}: {self.product_name}" 
--- hello/views.py
+++ hello/views.py
@@ -1,64 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
-posts = [
-    {
-        'productType': 'pork',
-        'author': 'HK Scan',
-        'product_name': 'Tenderloin Steak (Pork)',
-        'description' : 'succulent farm-grown Finnish pork tenderloin. We recommend roasting it.',
-        'Recipe': 'Our favorite is crackling pork, you can find the recipe here',
-        'RecipeLink': 'https://www.hk.fi/reseptit',
-        'media': 'https://hkruokatalo.studio.crasman.fi/pub/Kuvat/Tuotekuvat2/3661.jpg?c=system_1024x',
-        'Price' : '14.99',
-        'recipe_name': 'crackling pork',
-        'content': '111',
-        'date_posted': 'Nov 7 2020',
-        'recipe_main': 'https://www.hk.fi/reseptit/resepti/crackling-pork-joulukinkku/'
-
-    },
-    {
-        'productType': 'beef',
-        'author': 'HK Scan',
-        'product_name': 'Tenderloin Steak (Beef)',
-        'description': 'succulent farm-grown Finnish beef tenderloin. We making a burger!.',
-        'Recipe': 'We can back the classic burger, you can find the recipe here',
-        'Price': '10',
-        'media': 'https://hkruokatalo.studio.crasman.fi/pub/Kuvat/Tuotekuvat2/3437.jpg?c=system_1024x',
-        'recipe_main': 'https://www.hk.fi/reseptit/resepti/original-burger/',
-        'recipe_name' : 'original burger',
-        'content': '222',
-        'date_posted': 'Nov 7 2020'
-    },
-    {
-        'productType': 'chicken',
-        'author': 'HK Scan',
-        'product_name': 'Vegan Nuggets',
-        'description': 'the perfect fried snacks while making sustainable choices for the world',
-        'Price': '7',
-        'Recipe' : 'Eat them as is !',
-        'media': 'https://hkruokatalo.studio.crasman.fi/pub/Kuvat/Tuotekuvat2/7856.jpg?c = system_1024x',
-        'content': '333',
-        'date_posted': 'Nov 7 2020'
-    }
-    
-    # {
-    #     'author': 'HK Scan',
-    #     'title': 'Production location:',
-    #     'product_name': 'Tampere',
-    #     'media': '',
-    #     'content': '69420',
-    #     'date_posted': 'Nov 7 2020'
-    # },
-    # {
-    #     'author': 'HK Scan',
-    #     'title': 'Product name:',
-    #     'product_name': 'Tenderloin Steak',
-    #     'media': '',
-    #     'content': '69420',
-    #     'date_posted': 'Nov 7 2020'
-    # },
-]
+posts = Product.objects.all()
 
 def hello(request):
     context = {
